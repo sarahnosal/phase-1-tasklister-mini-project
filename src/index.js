964f0aca0816301c48eb10af31e75fae5f7ddbd9
Sarah Nosal <nosalsm@gmail.com>
2021-11-29 14:08:37 -0600

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    // const newTaskDescription = document.getElementById('new-task-description');
+    handleToDo(e.target.new_task_description.value);
+  })
 });
+function handleToDo(todo) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button');
+  btn.addEventListener('click', deleteTask);
+  btn.textContent = ' ✓';
+  li.textContent = `${todo} `;
+  li.appendChild(btn);
+  document.querySelector('#tasks').appendChild(li);
+}
+function deleteTask(e) {
+  e.target.parentNode.remove();
+}
